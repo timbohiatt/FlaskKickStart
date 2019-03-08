f669dd438886fa34f152978f770760df8e67c693
Tim Hiatt <timbohiatt@gmail.com>
2019-03-08 22:30:26 +0100

    Flask Kick Start

---
+++ web/web_boot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while true; do
+
+	if [[ ! -d "/migrations/" ]]; then
+   		echo "Not Exist"
+   		flask db init
+   		flask db migrate
+	fi
+
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Upgrade command failed, retrying in 5 secs...
+    sleep 5
+done
+
+exec gunicorn -w 4 -b 0.0.0.0:5000 --access-logfile - --error-logfile - run:app
